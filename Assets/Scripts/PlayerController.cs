89823f502c6fb986b90b127a56a5ad8dc82309fc
Thomas Sullivan <tdsullivan812@gmail.com>
2020-01-27 14:27:40 -0500

    Added sprites into Unity scene for testing purposes, and started unfinished drafts of Services Locator and player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private const float _moveForce = 10;
+    private
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+    }
+}
